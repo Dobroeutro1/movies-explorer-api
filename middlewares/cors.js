a62641f65fb5720d08c1e243c05c8204bb251c5d
Артем <63804700+Dobroeutro1@users.noreply.github.com>
2021-05-08 15:00:24 +0500

    cors

--- middlewares/cors.js
+++ middlewares/cors.js
@@ -1,10 +1,13 @@
 const cors = (req, res, next) => {
-  res.header('Access-Control-Allow-Origin', '*')
-  res.header(
+  res.setHeader('Access-Control-Allow-Origin', '*')
+  res.setHeader(
     'Access-Control-Allow-Headers',
     'Origin, X-Requested-With, Content-Type, Accept'
   )
-  res.header('Access-Control-Allow-Methods', 'GET,HEAD,PUT,PATCH,POST,DELETE')
+  res.setHeader(
+    'Access-Control-Allow-Methods',
+    'GET,HEAD,PUT,PATCH,POST,DELETE'
+  )
   next()
 }
 
