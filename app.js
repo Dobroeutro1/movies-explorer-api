f99af6f0d17878e758fdee683d7b8728f397f678
Артем <63804700+Dobroeutro1@users.noreply.github.com>
2021-05-08 14:39:57 +0500

    cors

--- app.js
+++ app.js
@@ -24,6 +24,17 @@ mongoose.connect(
   }
 )
 
+app.use((req, res, next) => {
+  res.header('Access-Control-Allow-Origin', '*')
+  res.header(
+    'Access-Control-Allow-Headers',
+    'Origin, X-Requested-With, Content-Type, Accept'
+  )
+  res.header('Access-Control-Allow-Methods', 'GET,HEAD,PUT,PATCH,POST,DELETE')
+
+  next()
+})
+
 app.use(helmet())
 
 app.use(requestLogger)
